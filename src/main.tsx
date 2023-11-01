aa54dfd6adf6a64699d69a2a3814094105488478
scribeNcode <deyeye99@gmail.com>
2023-11-01 05:19:33 +0100

    imported and wrapped ChakraProvider round App

--- src/main.tsx
+++ src/main.tsx
@@ -3,8 +3,13 @@ import ReactDOM from 'react-dom/client'
 import App from './App.tsx'
 import './index.css'
 
+//import chakraProvider and wrapped chakraProvider at the root of my app
+import { ChakraProvider } from '@chakra-ui/react'
+
 ReactDOM.createRoot(document.getElementById('root')!).render(
   <React.StrictMode>
-    <App />
+    <ChakraProvider>
+      <App />
+    </ChakraProvider>
   </React.StrictMode>,
 )
